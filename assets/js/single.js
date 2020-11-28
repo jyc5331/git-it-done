1f734077c016848f434e325ae90a3c79f351480b
jyc5331 <jcosgrovecoding@gmail.com>
2020-11-28 10:38:09 -0500

    6.3.6 complete, displayWarning function added

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
-
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
   console.log(repo);
@@ -10,6 +10,10 @@ var getRepoIssues = function (repo) {
     if (response.ok) {
       response.json().then(function (data) {
         displayIssues(data);
+        //check if api has paginated issues
+        if (response.headers.get("Link")) {
+            displayWarning(repo);
+        }
       });
     } else {
       alert("There was a problem with your request!");
@@ -50,5 +54,16 @@ var displayIssues = function (issues) {
     issueContainerEl.appendChild(issueEl);
   } // end of for loop
 }; // end of displayIssues function
+
+var displayWarning = function(repo) {
+    //add text to the warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 getRepoIssues("facebook/react");
